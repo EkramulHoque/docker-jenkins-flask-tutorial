511311d6eaf12461f202cbf5e5e015239e7a8dc6
EkramulHoque <ekramulhq26@gmail.com>
2019-03-08 00:55:10 -0800

    project added

---
+++ model.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+import pickle
+from  sklearn import  datasets
+iris=datasets.load_iris()
+x=iris.data
+y=iris.target
+
+#labels for iris dataset
+labels ={
+  0: "setosa",
+  1: "versicolor",
+  2: "virginica"
+}
+
+#split the data set
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=.5)
+
+#Using decision tree algorithm
+from sklearn import tree
+classifier=tree.DecisionTreeClassifier()
+classifier.fit(x_train,y_train)
+predictions=classifier.predict(x_test)
+
+#export the model
+pickle.dump(classifier, open('model.pkl','wb'))
+
+#load the model and test with a custom input
+model = pickle.load( open('model.pkl','rb'))
+x = [[6.7, 3.3, 5.7, 2.1]]
+predict = model.predict(x)
+print(labels[predict[0]])
---
+++ server.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, jsonify
+import json
+import pickle
+import pandas as pd
+import numpy as np
+
+app = Flask(__name__)
+
+# Load the model
+model = pickle.load(open('model.pkl','rb'))
+labels ={
+  0: "setosa",
+  1: "versicolor",
+  2: "virginica"
+}
+
+@app.route('/api',methods=['POST'])
+def predict():
+    # Get the data from the POST request.
+	data = request.get_json(force=True)
+	predict = model.predict(data['feature'])
+	return jsonify(predict[0].tolist())
+
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0')
