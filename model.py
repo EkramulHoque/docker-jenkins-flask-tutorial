b8d49c0d914436782ac20ca3509e4f3a5beef130
EkramulHoque <ekramulhq26@gmail.com>
2019-03-08 02:03:09 -0800

    updated model

--- model.py
+++ model.py
@@ -15,7 +15,7 @@ labels ={
 
 #split the data set
 from sklearn.model_selection import train_test_split
-x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=.5)
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=.25)
 
 #Using decision tree algorithm
 from sklearn import tree
